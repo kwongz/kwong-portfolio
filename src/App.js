a891ad6fd92dd82ac5e53d2094f27f75e5c18f86
Kwongz <kyle.wong917@gmail.com>
2024-01-19 14:46:26 -0500

    fixed homepage url

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import Home from "./Components/Home.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<Router>
-			<div className="App">
-				<NavBar />
-				<Routes>
-					<Route path="/kwongz-portfolio" element={<Home />} />
-					<Route path="/connect4" element={<Connect4 />} />
-					<Route path="/tic-tac-toe" element={<Tictactoe />} />
-				</Routes>
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/kwong-portfolio" element={<Home />} />
+          <Route path="/connect4" element={<Connect4 />} />
+          <Route path="/tic-tac-toe" element={<Tictactoe />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
