f576eab8dee3152e294fb47e9dd4f739977578e6
Kyle Wong <you@example.com>
2024-01-21 17:19:18 -0500

    finished responsive styling for contact button, added new project

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,35 +2,38 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
-  const [mobileNavClosed, setMobileNavClosed] = useState(true);
+	const [mobileNavClosed, setMobileNavClosed] = useState(true);
 
-  return (
-    <div className="navbar">
-      <h1>
-        <Link to="/kwong-portfolio">@Code Kyle Wong</Link>
-      </h1>
-      <div
-        className="hamburger-menu"
-        onClick={() => setMobileNavClosed(!mobileNavClosed)}
-      >
-        <i
-          className={`fa-solid fa-bars ${mobileNavClosed ? "" : "hidden"}`}
-        ></i>
-        <i className={`fa-solid fa-x ${mobileNavClosed ? "hidden" : ""}`}></i>
-      </div>
-      <ul className={`menu ${mobileNavClosed ? "" : "showMenu"}`}>
-        <li>
-          <Link to="/tic-tac-toe">Tic-tac-toe</Link>
-        </li>
-        <li>
-          <Link to="/connect4">Connect 4</Link>
-        </li>
-        <li className="contact">
-          <a href="mailto:Kyle.wong917@gmail.com">Contact</a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="navbar">
+			<h1>
+				<Link to="/kwong-portfolio">@Code Kyle Wong</Link>
+			</h1>
+			<div
+				className="hamburger-menu"
+				onClick={() => setMobileNavClosed(!mobileNavClosed)}>
+				<i
+					className={`fa-solid fa-bars ${mobileNavClosed ? "" : "hidden"}`}></i>
+				<i className={`fa-solid fa-x ${mobileNavClosed ? "hidden" : ""}`}></i>
+			</div>
+			<ul className={`menu ${mobileNavClosed ? "" : "showMenu"}`}>
+				<li>
+					<Link to="/tic-tac-toe">Tic-tac-toe</Link>
+				</li>
+				<li>
+					<Link to="/connect4">Connect 4</Link>
+				</li>
+				<li>
+					<Link target="_blank" to="https://kwongz.github.io/KW-ECommerce-App/">
+						Makeup store
+					</Link>
+				</li>
+				<li className="contact">
+					<a href="mailto:Kyle.wong917@gmail.com">Contact</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default NavBar;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,64 +1,103 @@
 import React from "react";
 import connect4Pic from "../images/connect4.png";
 import ticTacToePic from "../images/tic-tac-toe.png";
+import kwEcommerce from "../images/kw-ecommerce.png";
 
 function Projects({ projectHidden }) {
-  return (
-    <div className={`projects-section ${projectHidden ? "hidden" : ""}`}>
-      <div className="project-grid-container">
-        <a href="/tic-tac-toe">
-          <div className="project-container">
-            <h3>Tic-Tac-Toe</h3>
-            <div className="project-image-container">
-              <img src={ticTacToePic} />
-            </div>
-            <div className="project-description-container">
-              <h4>React Project</h4>
-              <p>
-                A <span className="copy-highlight">User Friendly</span>{" "}
-                tic-tac-toe game, with multiple{" "}
-                <span className="copy-highlight">Reusuable Components</span> for
-                future projects.
-              </p>
-              <a
-                href="https://github.com/kwongz/kwong-portfolio/tree/master/src/gameComponents"
-                target="_blank"
-              >
-                Code
-              </a>
-              <a href="/tic-tac-toe">Link</a>
-            </div>
-          </div>
-        </a>
-        <a href="/connect4">
-          <div className="project-container">
-            <h3>Connect 4</h3>
-            <div className="project-image-container">
-              <img src={connect4Pic} />
-            </div>
-            <div className="project-description-container">
-              <h4>React Project</h4>
-              <p>
-                <span className="copy-highlight">Repurposing</span>{" "}
-                <span className="copy-highlight">components</span> and using a
-                directional{" "}
-                <span className="copy-highlight">recursive function</span> as my
-                checkWin logic. Reuitlizing the recursion for upcoming project
-              </p>
+	return (
+		<div className={`projects-section ${projectHidden ? "hidden" : ""}`}>
+			<div className="project-grid-container">
+				<div className="project-container">
+					<h3>Tic-Tac-Toe</h3>
+					<div className="project-image-container">
+						<a href="/tic-tac-toe" target="_blank">
+							<img src={ticTacToePic} />
+						</a>
+					</div>
+					<div className="project-description-container">
+						<h4>React Project</h4>
+						<p>
+							A <span className="copy-highlight">User Friendly</span>{" "}
+							tic-tac-toe game, with multiple{" "}
+							<span className="copy-highlight">Reusuable Components</span> for
+							future projects.
+						</p>
+						<a
+							className="project-link"
+							href="https://github.com/kwongz/kwong-portfolio/tree/master/src/gameComponents"
+							target="_blank">
+							Code
+						</a>
+						<a className="project-link" href="/tic-tac-toe" target="_blank">
+							Link
+						</a>
+					</div>
+				</div>
+				<div className="project-container">
+					<h3>Connect 4</h3>
+					<div className="project-image-container">
+						<a href="/connect4" target="_blank">
+							<img src={connect4Pic} />
+						</a>
+					</div>
+					<div className="project-description-container">
+						<h4>React Project</h4>
+						<p>
+							<span className="copy-highlight">Repurposing</span>{" "}
+							<span className="copy-highlight">components</span> and using a
+							directional{" "}
+							<span className="copy-highlight">recursive function</span> as my
+							checkWin logic. Reuitlizing the recursion for upcoming project
+						</p>
 
-              <a
-                href="https://github.com/kwongz/kwong-portfolio/tree/master/src/gameComponents"
-                target="_blank"
-              >
-                Code
-              </a>
-              <a href="/connect4">Link</a>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+						<a
+							className="project-link"
+							href="https://github.com/kwongz/kwong-portfolio/tree/master/src/gameComponents"
+							target="_blank">
+							Code
+						</a>
+						<a className="project-link" href="/connect4" target="_blank">
+							Link
+						</a>
+					</div>
+				</div>
+				<div className="project-container">
+					<h3>Make-up Store</h3>
+					<div className="project-image-container">
+						<a
+							href="https://kwongz.github.io/KW-ECommerce-App/"
+							target="_blank">
+							<img src={kwEcommerce} />
+						</a>
+					</div>
+					<div className="project-description-container">
+						<h4>React Project</h4>
+						<p>
+							<span className="copy-highlight">API Request</span> to populate
+							store inventory, <span className="copy-highlight">Modal</span>{" "}
+							handling for favourites and cart store, brand and type{" "}
+							<span className="copy-highlight">Filtering</span> for store
+							inventory. <span className="copy-highlight">UseContext</span> to
+							simply passing from children components
+						</p>
+
+						<a
+							className="project-link"
+							href="https://github.com/kwongz/KW-ECommerce-App"
+							target="_blank">
+							Code
+						</a>
+						<a
+							className="project-link"
+							href="https://kwongz.github.io/KW-ECommerce-App/"
+							target="_blank">
+							Link
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Projects;
