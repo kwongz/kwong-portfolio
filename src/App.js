ca37aab4fe4a509aaeed3795a580a7a235409124
Kwongz <kyle.wong917@gmail.com>
2024-02-01 16:47:12 -0500

    adding hashrouter, fixed nav styling and hover/focus effects

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import NavBar from "./Components/NavBar.jsx";
 import Tictactoe from "./gameComponents/Tictactoe.jsx";
 import Connect4 from "./gameComponents/Connect4.jsx";
 import Home from "./Components/Home.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/kwong-portfolio" element={<Home />} />
-          <Route path="/kwong-portfolio/connect4" element={<Connect4 />} />
-          <Route path="/kwong-portfolio/tic-tac-toe" element={<Tictactoe />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/kwong-portfolio" element={<Home />} />
+        <Route path="/kwong-portfolio/connect4" element={<Connect4 />} />
+        <Route path="/kwong-portfolio/tic-tac-toe" element={<Tictactoe />} />
+      </Routes>
+    </div>
   );
 }
 
